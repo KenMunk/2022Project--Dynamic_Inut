76faf583154b4325d07b29a0de19f6f414d34197
Kenneth Munk <contact.me@kenmunk.com>
2022-05-28 01:11:40 -0700

    input subscriber draft 1 finished
    needs testing

--- Assets/DynamicInput/InputSubscriber.cs
+++ Assets/DynamicInput/InputSubscriber.cs
@@ -12,18 +12,27 @@ public class InputSubscriber : MonoBehaviour
     private Vector3Action   vec3Acts;
     private Vector4Action   vec4Acts;
 
-    private bool xbool = true;
-    private float xfloat = 1;
-    private Vector2 xVector2 = new Vector2();
-    private Vector3 xVector3 = new Vector3();
-    private Vector4 xVector4 = new Vector4();
+    private InputActivatedAction subscriberActions;
+    private ActionTest testActions;
+
+    private string watchedAction = "";
+    public PlayerInput watchedInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerInput testInput = GetComponent<PlayerInput>();
+        if(watchedInput == null)
+        {
+            watchedInput = GetComponent<PlayerInput>();
+        }
 
-        testInput.actions["WASD"].performed += evaluateInput;
+        subscriberActions += evaluateInput;
+        subscriberActions += isActivated;
+        subscriberActions += tryEvaluateBoolInput;
+        subscriberActions += tryEvaluateFloatInput;
+        subscriberActions += tryEvaluateVector2Input;
+        subscriberActions += tryEvaluateVector3Input;
+        subscriberActions += tryEvaluateVector4Input;
     }
 
     // Update is called once per frame
@@ -32,7 +41,31 @@ public class InputSubscriber : MonoBehaviour
         
     }
 
+    public void setWatchedAction(string action)
+    {
+        if(watchedInput != null)
+        {
+            if(watchedAction == "")
+            {
+                watchedInput.actions[watchedAction].performed += invokeSubscriberActions;
+            }
+            else
+            {
+                watchedInput.actions[watchedAction].performed -= invokeSubscriberActions;
 
+                watchedAction = action;
+
+
+                watchedInput.actions[watchedAction].performed += invokeSubscriberActions;
+
+            }
+        }
+    }
+
+    public void invokeSubscriberActions(InputAction.CallbackContext context)
+    {
+        subscriberActions?.Invoke(context);
+    }
 
     public void evaluateInput(InputAction.CallbackContext context)
     {
@@ -40,19 +73,83 @@ public class InputSubscriber : MonoBehaviour
         System.Type valueType = context.valueType;
 
         Debug.LogFormat($"Input type: {valueType} with value of {context.ReadValueAsObject()}");
+    }
+
+    public void isActivated(InputAction.CallbackContext context)
+    {
+        testActions?.Invoke();
+    }
+
+    public bool evaluatesAs(InputAction.CallbackContext context, System.Type asType)
+    {
+        System.Type valueType = context.valueType;
 
         var value = context.ReadValueAsObject();
 
-        //These could probably be broken into individual functions which would allow for them all to be called in parallel since they're all type dependent.
-        if (valueType.IsInstanceOfType(xbool))
+        return (valueType.Equals(asType));
+    }
+
+    public void tryEvaluateBoolInput(InputAction.CallbackContext context)
+    {
+        if (evaluatesAs(context, (true).GetType()))
         {
-            Debug.Log("Bool detected");
-            boolActs?.Invoke((bool)value);
-            return;
+            boolActs?.Invoke((bool)(context.ReadValueAsObject()));
         }
     }
 
-    private void addBoolActs(BooleanAction acts)
+    public void tryEvaluateFloatInput(InputAction.CallbackContext context)
+    {
+        if(evaluatesAs(context, (0.1f).GetType()))
+        {
+            floatActs?.Invoke((float)(context.ReadValueAsObject()));
+        }
+    }
+    
+    public void tryEvaluateVector2Input(InputAction.CallbackContext context)
+    {
+        if (evaluatesAs(context, (new Vector2()).GetType()))
+        {
+            vec2Acts?.Invoke((Vector2)(context.ReadValueAsObject()));
+        }
+    }
+
+    public void tryEvaluateVector3Input(InputAction.CallbackContext context)
+    {
+        if (evaluatesAs(context, (new Vector3()).GetType()))
+        {
+            vec3Acts?.Invoke((Vector3)(context.ReadValueAsObject()));
+        }
+    }
+
+    public void tryEvaluateVector4Input(InputAction.CallbackContext context)
+    {
+        if (evaluatesAs(context, (new Vector4()).GetType()))
+        {
+            vec4Acts?.Invoke((Vector4)(context.ReadValueAsObject()));
+        }
+    }
+
+    public void addActionTest(ActionTest test)
+    {
+        if(testActions == null)
+        {
+            testActions = test;
+        }
+        else
+        {
+            testActions += test;
+        }
+    }
+    
+    public void removeActionTest(ActionTest test)
+    {
+        if (testActions != null)
+        {
+            testActions -= test;
+        }
+    }
+
+    public void addBoolActs(BooleanAction acts)
     {
         if(boolActs == null)
         {
@@ -64,5 +161,91 @@ public class InputSubscriber : MonoBehaviour
         }
     }
 
+    public void removeBoolActs(BooleanAction acts)
+    {
+        if(boolActs != null)
+        {
+            boolActs -= acts;
+        }
+    }
+
+    public void addFloatActs(FloatAction acts)
+    {
+        if(floatActs == null)
+        {
+            floatActs = acts;
+        }
+        else
+        {
+            floatActs += acts;
+        }
+    }
+
+    public void removeFloatActs(FloatAction acts)
+    {
+        if(floatActs != null)
+        {
+            floatActs -= acts;
+        }
+    }
+
+    public void addVec2Acts(Vector2Action acts)
+    {
+        if(vec2Acts == null)
+        {
+            vec2Acts = acts;
+        }
+        else
+        {
+            vec2Acts += acts;
+        }
+    }
+
+    public void removeVec2Acts(Vector2Action acts)
+    {
+        if(vec2Acts != null)
+        {
+            vec2Acts -= acts;
+        }
+    }
+
+    public void addVec3Acts(Vector3Action acts)
+    {
+        if(vec3Acts == null)
+        {
+            vec3Acts = acts;
+        }
+        else
+        {
+            vec3Acts += acts;
+        }
+    }
+
+    public void removeVec3Acts(Vector3Action acts)
+    {
+        if (vec3Acts != null)
+        {
+            vec3Acts -= acts;
+        }
+    }
     
+    public void addVec4Acts(Vector4Action acts)
+    {
+        if (vec4Acts == null)
+        {
+            vec4Acts = acts;
+        }
+        else
+        {
+            vec4Acts += acts;
+        }
+    }
+
+    public void removeVec4Acts(Vector4Action acts)
+    {
+        if (vec4Acts != null)
+        {
+            vec4Acts -= acts;
+        }
+    }
 }
